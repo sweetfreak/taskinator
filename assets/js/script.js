c654cbfc1e11bb63aecb3e9c207632125980c478
jesse sheehan <jessetsheehan@gmail.com>
2022-05-05 18:51:25 -0400

    add categories and bolded tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,33 @@
+//top form element in header that you type into
 var formEl = document.querySelector("#task-form");
+//"tasks to do" element - the new boxes
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+//function to create a new task
 var createTaskHandler = function() {
+    //prevents page from refreshing/default browser scripts from running
   event.preventDefault();
+  //creates new variable for whatever you type into the formEl, and puts it in the box
+  //notice the ".value" at the end
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
   
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  //creates an li item in the html
   var listItemEl = document.createElement("li");  
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  //create dic to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  //give it a class name
+  taskInfoEl.className = "task-info";
+  //add hTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  //listItemEl.textContent = taskNameInput;
+
+  //adds li element to the list
   tasksToDoEl.appendChild(listItemEl);
 
 }
